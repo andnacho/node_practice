dd06343676a5093cbb60d9bbb9048394dea966e1
Nacho <andres@mail.com>
2021-07-24 18:37:30 -0500

    se agregan otras vistas

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product'
     });
@@ -15,11 +15,11 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
-        res.render('shop', {
+        res.render('/products', {
             prods: products,
-            pageTitle: 'Shop',
-            path: '/',
+            pageTitle: 'All products',
+            path: '/products',
             hasProducts: products.length > 0
         });
     });
-}
+};
--- models/product.js
+++ models/product.js
@@ -7,31 +7,34 @@ const p = path.join(
     'product.json'
 );
 
+const getProductsFromFile = cb => {
+    
+    fs.readFile(p, (err, data) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(data))
+        }
+    });
+};
+
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        fs.readFile(p, (err, data) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(data);
-            }
+        getProductsFromFile(products => {
             products.push(this)
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
+            
         });
     }
 
     static fetchAll(cb) {
-        let products = [];
-        fs.readFile(p, (err, data) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(data))
-        });
+        getProductsFromFile(cb);
     }
 }
